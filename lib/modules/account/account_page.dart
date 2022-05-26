0f7e7b8334634b0613f9fc89c3588ed8a5cf07d0
zhusw <sink50@foxmail.com>
2022-05-26 17:13:16 +0800

    个人中心 主体页面结构

--- lib/modules/account/account_page.dart
+++ lib/modules/account/account_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({Key? key}) : super(key: key);
@@ -10,8 +11,58 @@ class AccountPage extends StatefulWidget {
 class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('我的'),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            actions: <Widget>[
+              Container(
+                child: const Text("我的"),
+              )
+            ],
+            backgroundColor: Colors.deepOrange,
+            expandedHeight: 200 + MediaQuery.of(context).padding.top,
+            pinned: true,
+            flexibleSpace: Container(
+              color: Colors.amber,
+            ),
+          ),
+          _MenusWidget()
+        ],
+      ),
     );
   }
 }
+
+class _MenusWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTileTheme(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 30),
+      child: SliverList(
+        delegate: SliverChildListDelegate(_menus()),
+      ),
+    );
+  }
+
+  List<Widget> _menus(){
+    var iconColor = Colors.red;
+
+    final lws = <Widget>[];
+    for(int i = 0;i<100;i++){
+      lws.add(ListTile(
+        title: Text("title:$i"),
+        onTap: ()=>{
+
+        },
+        leading: Icon(
+          Icons.settings,
+          color: iconColor,
+        ),
+        trailing: const Icon(Icons.chevron_right),
+      ));
+    }
+    return lws;
+  }
+}
+
