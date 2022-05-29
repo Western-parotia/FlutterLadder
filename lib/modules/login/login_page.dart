711f079246ba38a235692a4f231f3382ca0375ce
zhusw <sink50@foxmail.com>
2022-05-29 18:05:07 +0800

    第三方登录 布局

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -11,13 +11,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   @override
   Widget build(BuildContext context) {
     final titleBarHeight = 56 + MediaQuery.of(context).padding.top;
 
     return Scaffold(
-      body: Stack(
+        body: Stack(
       children: <Widget>[
         Flex(
           direction: Axis.vertical,
@@ -31,57 +30,82 @@ class _LoginPageState extends State<LoginPage> {
             Expanded(
                 flex: 1,
                 child: Container(
-                  color: Colors.blueGrey,
+                  color: Colors.grey.shade200,
                 )),
           ],
         ),
         Column(
           children: <Widget>[
-            SizedBox( // 关闭键
+            SizedBox(
+              // 关闭键
               height: titleBarHeight,
               child: Container(
-                padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top,left:MediaQuery.of(context).padding.left),
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).padding.top,
+                    left: MediaQuery.of(context).padding.left),
                 color: Colors.black26,
-                child:const Align(
+                child: const Align(
                   alignment: Alignment.centerLeft,
-                  child:CloseButton(
+                  child: CloseButton(
                     color: Colors.white,
                   ),
-                ) ,
+                ),
               ),
             ),
-          Container(
-            padding: const EdgeInsets.only(top: 30),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Column(
-                children: <Widget>[
-                  Hero(tag: "_logo", child: SizedBox(
-                    height: 130,
+            Container(
+              // logo
+              padding: const EdgeInsets.only(top: 30),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Column(
+                  children: <Widget>[
+                    Hero(
+                        tag: "_logo",
+                        child: SizedBox(
+                          height: 130,
+                          width: 100,
+                          child: Image.asset(
+                            ImagesPath.wrapLocalImage("login_logo.png"),
+                            colorBlendMode: BlendMode.srcIn,
+                            fit: BoxFit.fitWidth,
+                            color: Colors.white,
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    color: Colors.grey,
+                    height: 1.5,
                     width: 100,
-                    child: Image.asset(
-                      ImagesPath.wrapLocalImage("login_logo.png"),
-                      colorBlendMode: BlendMode.srcIn,
-                      fit: BoxFit.fitWidth,
-                      color: Colors.white,
-                    ),
-                  ) ),
-
-                  SizedBox(
-
                   ),
-                ],
-              ),
-            ),
-          )
-
-
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: const Text(
+                    "第三方登录",
+                    style: TextStyle(color: Colors.grey, fontSize: 15),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    color: Colors.grey,
+                    height: 1.5,
+                    width: 100,
+                  ),
+                ),
+              ],
+            )
           ],
         )
-
       ],
-    )
-    );
-
+    ));
   }
 }
