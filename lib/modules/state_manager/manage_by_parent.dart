13d91a83ccc8c2e4182f1df6a0a3888b5333ad8d
zhusw <sink50@foxmail.com>
2022-06-02 15:16:09 +0800

    优化图标

--- lib/modules/state_manager/manage_by_parent.dart
+++ lib/modules/state_manager/manage_by_parent.dart
@@ -12,86 +12,52 @@ class ParentWidget extends StatefulWidget {
 }
 
 class _ParentWidgetState extends State<ParentWidget> {
-  final tag = "TapboxAState";
-  bool _active = false;
+  bool _tapBActive = false;
 
-  void _onTap() {
+  void _onTapBClick(bool newValue) {
     setState(() {
-      _active = !_active;
+      _tapBActive = newValue;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    "build before super".logI(tag);
+    return Scaffold(
+      body: TapboxB(active: _tapBActive, onClick: _onTapBClick),
+    );
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  final tag = "TapboxB";
+
+  final ValueChanged<bool> onClick;
+  bool active = false;
+
+  TapboxB({Key? key, this.active = false, required this.onClick})
+      : super(key: key);
+
+  void _onTap() {
+    onClick(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    "build ".logI(tag);
     return GestureDetector(
       onTap: _onTap,
       child: Container(
-        color: _active ? Colors.greenAccent : Colors.grey,
+        width: 300,
+        height: 500,
         child: Center(
           child: Text(
-            _active ? "Click to Inactive" : "Click to Active",
+            active ? "Click to Inactive" : "Click to Active",
             style: const TextStyle(fontSize: 30, color: Colors.white),
           ),
         ),
+        decoration: BoxDecoration(
+            color: active ? Colors.yellow[900] : Colors.grey[400]),
       ),
     );
-  }
-
-  ///生命周期函数监听----------------------------------------------------------
-  @override
-  void initState() {
-    "initState before super".logI(tag);
-    super.initState();
-    "initState after super".logI(tag);
-  }
-
-  @override
-  void didChangeDependencies() {
-    "didChangeDependencies before super".logI(tag);
-    super.didChangeDependencies();
-    "didChangeDependencies after super".logI(tag);
-  }
-
-  @override
-  void didUpdateWidget(covariant ParentWidget oldWidget) {
-    "didUpdateWidget before super".logI(tag);
-    super.didUpdateWidget(oldWidget);
-    "didUpdateWidget after super".logI(tag);
-  }
-
-  @override
-  void activate() {
-    "didUpdateWidget before super".logI(tag);
-    super.activate();
-    "activate after super".logI(tag);
-  }
-
-  @override
-  void deactivate() {
-    "didUpdateWidget before super".logI(tag);
-    super.deactivate();
-    "deactivate after super".logI(tag);
-  }
-
-  @override
-  void dispose() {
-    "dispose before super".logI(tag);
-    super.dispose();
-    "dispose after super".logI(tag);
-  }
-
-  @override
-  void setState(VoidCallback fn) {
-    "setState before super".logI(tag);
-    super.setState(fn);
-    "setState after super".logI(tag);
-  }
-
-  @override
-  void reassemble() {
-    "reassemble before super".logI(tag);
-    super.reassemble();
-    "reassemble after super".logI(tag);
   }
 }
