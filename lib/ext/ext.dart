ce1bcce94490ea775ec474c27db1333d6f31ab57
zhusw <sink50@foxmail.com>
2022-06-02 14:17:07 +0800

    优化日志

--- lib/ext/ext.dart
+++ lib/ext/ext.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:wanandroid_app/utils/log_utils.dart';
 
 extension StringExt on String {
   /// 通过在相邻的字符中插入不可见的幽灵字符，避免系统识别为完整单词或连续数字而断句
@@ -8,6 +7,6 @@ extension StringExt on String {
   }
 
   void logI(String? tag) {
-    Log.i("$tag:-:$this");
+      print("$tag:-:$this");
   }
 }
