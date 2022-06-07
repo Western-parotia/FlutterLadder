a52c2d5193c0461bae23e30d1cca1c598ca9c382
韩冬 <413801445@qq.com>
2022-06-07 19:24:43 +0800

    add 登录页面绘制完成，还差一个圆弧

--- lib/pages/login/hd_login_page.dart
+++ lib/pages/login/hd_login_page.dart
@@ -9,19 +9,25 @@ class HDLoginPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => HDLoginPageState();
 }
-
 class HDLoginPageState extends State<HDLoginPage> {
+  // 头部蓝色背景
+  double topBgViewH = 250;
+  // 账号密码输入View 高度
+  double inputViewH = 250;
+  // 登录图标高度
+  double loginIconH = 80;
+  //double inputViewW = Screen.width(context) - 80;
+  // 关闭按钮高度
+  double closeIconH = 25;
 
-  /// 账号输入控制器
+  // 账号输入控制器
   TextEditingController accountController = TextEditingController()..addListener(() {
 
   });
-
-  /// 密码输入控制器
+  // 密码输入控制器
   TextEditingController passwordController = TextEditingController()..addListener(() {
 
   });
-
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -29,35 +35,70 @@ class HDLoginPageState extends State<HDLoginPage> {
     );
   }
 
-  /// 账号密码输入框 登录按钮
+  // 账号密码输入框 登录按钮
   Widget getLoginSubViews() {
     double width = Screen.width(context);
     return Stack(
        children: [
-         /// 关闭图标
+         getBackGroundView(),
+         // 关闭按钮
+         Container(margin: EdgeInsets.only(left: 20,top: 35),
+           width: closeIconH,
+           height: closeIconH,
+           child: InkWell(child: Image.asset("images/hd_white_close.png"),
+             onTap:() {
+             }),),
+         getContentSubViews(),
+         /**
+         // 关闭图标
          Positioned(left: 20,top: 40,width: 30,height: 30,child: InkWell(child: Image.asset("images/icon_flutter.png"),
              onTap:() {
              })),
-         /// 登录Icon
+         // 登录Ico
          Positioned(left: (width-80.0)/2,top: 80,width: 80,height: 80,child: Image.asset("images/icon_flutter.png")),
-         /// 间隔
+         // 间隔
          Positioned(left: 0,top: 160,right: 0,height: 50,
              child: Container(
              )
          ),
-         /// 账号密码输入View
+         // 账号密码输入View
          Positioned(left:40,right: 40,top: 210,height: 250,child: getInputView())
+             */
        ],
     );
   }
 
-  /// 账号密码输入View
+  // 视图背景
+  Widget getBackGroundView () {
+    return Column(
+        children: [
+           Container(width: Screen.width(context),height: topBgViewH,color: Colors.blue,),
+           Padding(child: Container(color: Colors.grey,),padding: EdgeInsets.all(0),)
+        ],
+    );
+  }
+
+  // 登录页面子View
+  Widget getContentSubViews () {
+    return Column(
+       children: [
+          Container(margin: EdgeInsets.only(top: 80),width: loginIconH,height: loginIconH,child: Image.asset("images/icon_flutter.png"),),
+          Padding(padding: EdgeInsets.only(top: 40),child: getInputView(),),
+          Padding(padding: EdgeInsets.only(top: 30),child: getThirdLoginTipView()),
+          Padding(padding: EdgeInsets.only(top: 20,left: 0,right: 0),child: getThirdLoginTypeView(),)
+       ],
+    );
+  }
+
+  // 账号密码输入View
   Widget getInputView () {
     return Container(
+      height: inputViewH,
+      margin: EdgeInsets.only(left: 30,right: 30),
       decoration: const BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(5)),
         color: Colors.white,
-        /// blurRadius: 阴影模糊程度  spreadRadius: 1.0 //阴影扩散程度
+        // blurRadius: 阴影模糊程度  spreadRadius: 1.0 //阴影扩散程度
         boxShadow: [BoxShadow(color: Colors.black12, offset: Offset(1.0, 1.0), blurRadius: 15, spreadRadius: 3.0) ],
       ),
       //color: Colors.white,
@@ -78,7 +119,7 @@ class HDLoginPageState extends State<HDLoginPage> {
                  minimumSize: const Size(double.infinity, 40),
                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
              ),onPressed:() {
-               /// 登录按钮点击
+               // 登录按钮点击
              },)),),
            RichText(text: TextSpan(
              text: "还没账号？",
@@ -97,7 +138,7 @@ class HDLoginPageState extends State<HDLoginPage> {
     );
   }
 
-  /// 账号输入框
+  // 账号输入框
   Widget getAccountTextField() {
     return TextFormField(
       controller: accountController,
@@ -108,7 +149,7 @@ class HDLoginPageState extends State<HDLoginPage> {
     );
   }
 
-  /// 密码输入框
+  // 密码输入框
   Widget getPasswordTextField() {
     return TextFormField(
       controller: passwordController,
@@ -119,4 +160,98 @@ class HDLoginPageState extends State<HDLoginPage> {
           suffixIcon: Icon(Icons.lock)),
     );
   }
+
+  // 第三方登录View
+  Widget getThirdLoginView () {
+    return Column(
+      children: [
+        getThirdLoginTipView(),getThirdLoginTypeView()
+      ],
+    );
+  }
+  // 第三方登录提示View
+  Widget getThirdLoginTipView () {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(width: 60,height: 1,color: Colors.grey),
+          Padding(padding: EdgeInsets.only(left: 30,right: 30),child: Text("第三方登录",style: TextStyle(color: Colors.grey),),),
+          Container(width: 60,height: 1,color: Colors.grey)
+        ],
+      );
+  }
+
+  // 第三方登录方式View
+  Widget getThirdLoginTypeView () {
+    double ratio = Screen.width(context)/2/5;
+    //print("$ratio");
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        InkWell(child: Image.asset("images/icon_flutter.png",width: 50,height: 50,),
+            onTap:() {
+            }),
+        InkWell(child: Image.asset("images/icon_flutter.png",width: 50,height: 50),
+            onTap:() {
+            })
+      ],
+    );
+    /**
+    return Container(
+      color: Colors.red,
+      height: 50,
+      child: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, //横轴三个子widget
+              childAspectRatio: ratio//宽高比为1时，子widget
+          ),
+          children:<Widget>[
+            Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0),child: Icon(Icons.ac_unit),),
+            Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0),child: Icon(Icons.airport_shuttle),)
+            // Icon(Icons.ac_unit),
+            // Icon(Icons.airport_shuttle),
+            // Icon(Icons.all_inclusive),
+            // Icon(Icons.beach_access),
+            // Icon(Icons.cake),
+            // Icon(Icons.free_breakfast)
+          ]
+      ),
+    );*/
+  /**
+    return GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, //横轴三个子widget
+            childAspectRatio: 1.0 //宽高比为1时，子widget
+        ),
+        children:<Widget>[
+          Icon(Icons.ac_unit),
+          Icon(Icons.airport_shuttle),
+          Icon(Icons.all_inclusive),
+          Icon(Icons.beach_access),
+          Icon(Icons.cake),
+          Icon(Icons.free_breakfast)
+        ]
+    );
+      */
+    // return GridView.count(crossAxisCount: 2,childAspectRatio: 1.0,children: [
+    //   Icon(Icons.ac_unit),Icon(Icons.free_breakfast)
+    // ],);
+    /**
+    return Container(
+      color: Colors.red,
+      height: 50,
+      child: GridView.count(
+        //scrollDirection:Axis.horizontal,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,childAspectRatio: 0.5),
+        children:<Widget> [Icon(Icons.ac_unit),Icon(Icons.free_breakfast)],
+      ),
+    );
+      return GridView(
+          scrollDirection:Axis.horizontal,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,childAspectRatio: 0.5),
+          children:<Widget> [Icon(Icons.ac_unit),Icon(Icons.free_breakfast)],
+      );
+        */
+  }
+
 }
