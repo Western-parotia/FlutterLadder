7dfca7ffa59d955df68b482f62dfbcb2fe6e0779
zhusw <sink50@foxmail.com>
2023-04-10 16:02:35 +0800

    解决冲突

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -109,7 +109,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    super.build(context);
     return Scaffold(
       body: MediaQuery.removePadding(
         context: context,
